0e02947be1f533a79b358cc875d971bd157654a8
joycetaoyuuu <yt560@georgetown.edu>
2024-07-19 14:27:59 -0400

    updated README file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Caption Translation using Python
+# Caption Generation using Python
+
+This project provides a Python-based solution for extracting audio from a video, transcribing the audio into captions, translating those captions into another language, and adding the translated captions back into the video. The process leverages several libraries including ffmpeg, googletrans, and faster_whisper.
+
+### Requirements
+- Python 3.11
+- ffmpeg
+- googletrans
+- faster_whisper
+
+
+### Reference
+[https://python.plainenglish.io/polyglot-captions-transforming-multilingual-content-with-python-powered-caption-generation-7036da70d834](https://python.plainenglish.io/polyglot-captions-transforming-multilingual-content-with-python-powered-caption-generation-7036da70d834)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# extract the caption words from a short video
-# https://python.plainenglish.io/polyglot-captions-transforming-multilingual-content-with-python-powered-caption-generation-7036da70d834
 
+# reference: https://python.plainenglish.io/polyglot-captions-transforming-multilingual-content-with-python-powered-caption-generation-7036da70d834
 
+# packages
 import ffmpeg
 import os
 import time
@@ -10,83 +10,121 @@ from faster_whisper import WhisperModel
 
 class AudioGenerator:
     """
-    Extract audio from video.
+    Get audio from video.
     """
 
     def __init__(self):
         pass
 
     def get_audio(self,video):
+        """
+        Extracts audio from video and save the audio in the mp3 format.
+        Parameters:
+            video (str): The name of the video file.
+        Returns:
+            str: The name of the audio file.
+        """
         self.video = video
         audio = f"{os.path.splitext(self.video)[0]}_audio.mp3"
         
+        # use ffmpeg to extract audio from the input video
         ffmpeg.input(self.video).output(audio).run(overwrite_output=True)
+
         return audio
 
         
 class AudioToCaptions:
     """
-    Transcribe captions from audio using speech recognition model Whisper.
+    Get captions from audio.
     """
 
     def __init__(self):
         pass
 
     def transcribe(self,audio):
+        """
+        Transcribes captions from audio using speech recognition model Whisper.
+        Parameters:
+            audio (str): The name of audio file.
+        Returns:
+            list: List of segments of captions.
+        """
         self.audio = audio
-        model = WhisperModel("small")
-        segments, info = model.transcribe(audio)
+        model = WhisperModel("small") # load the whisper model
+        segments, info = model.transcribe(audio) # audio to text
         language = info[0]
         segments = list(segments)
         return segments
     
 
 
-# translate the caption words into english
-
 class CaptionTranslator:
+    """
+    Translate captions to another language.
+    """
 
     def __init__(self,caption_segments):
+        """
+        Parameters:
+            segments (list): List of segments of the captions.
+        """
         self.segments = caption_segments
 
     def translate(self,language):
+        """
+        Translates the captions to another language using googletrans.
+        Parameters:
+            language (str): the destination language
+        Returns:
+            str: the name of the translated caption file.
+        """
 
-        translator = Translator()
+        translator = Translator() # load the translator
 
-        subtitle_file = f'subtitle.{language}.srt'
+        subtitle_file = f'subtitle.{language}.srt' # define the return file
         text = ""
+        # go through each segment
         for index, segment in enumerate(self.segments):
-            segment_start = time.strftime('%H:%M:%S,000',time.gmtime(segment.start))
-            segment_end = time.strftime('%H:%M:%S,000',time.gmtime(segment.end))
+            segment_start = time.strftime('%H:%M:%S,000',time.gmtime(segment.start)) # reformat the timestamp
+            segment_end = time.strftime('%H:%M:%S,000',time.gmtime(segment.end)) # reformat the timestamp
             text += f"{str(index + 1)} \n"
             text += f"{segment_start} --> {segment_end} \n"
-            translated_text = translator.translate(segment.text,dest=language)
-            text += f"{translated_text.text} \n"
+            translated_text = translator.translate(segment.text,dest=language) # translate the text
+            text += f"{translated_text.text} \n" # add to text
             text += "\n"
         
-        with open(subtitle_file, "w", encoding="utf-8") as f:
+        with open(subtitle_file, "w", encoding="utf-8") as f: # save to the file
             f.write(text)
 
         return subtitle_file
 
 
-# replace the caption in the video to the tanslated words
-
 
 class AddSubtitle:
+    """
+    Add the subtitle to the video.
+    """
 
     def __init__(self,video):
         self.video = video
 
-
     def add_sub(self,subtitle):
+        """
+        Add the given subtitle to the video.
+        Parameters:
+            subtitle (str): The name of the subtitle.
+        Returns:
+            str: The name of the video with new subtitle.
+        """
         self.subtitle = subtitle
 
         output_file = f"{self.video.replace('.mp4','')}_with_captions.mp4"
 
         video_stream = ffmpeg.input(self.video)
 
-        ffmpeg.concat(video_stream.filter("subtitles",self.subtitle),video_stream.audio,v=1,a = 1).output(output_file).run(overwrite_output=True)
+        # add subtitle and audio to the video
+        style = "Fontsize=23,BackColour=&H80000000,BorderStyle=3"
+        ffmpeg.concat(video_stream.filter("subtitles",self.subtitle,force_style = style),video_stream.audio,v=1,a = 1).output(output_file).run(overwrite_output=True)
 
         return output_file
 
